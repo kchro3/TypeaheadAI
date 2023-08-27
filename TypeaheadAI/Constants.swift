2def84d3301e1aaabcee86624a8e00473909fac6
kchro3 <jhara1418@gmail.com>
2023-08-27 09:15:58 -0700

    check in

---
+++ TypeaheadAI/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TypeaheadAI
+//
+//  Created by Jeff Hara on 8/20/23.
+//
+
+import Foundation
+import KeyboardShortcuts
+
+extension KeyboardShortcuts.Name {
+    static let specialCopy = KeyboardShortcuts.Name("specialCopy", default: Shortcut(.c, modifiers: [.command, .control]))
+    static let specialPaste = KeyboardShortcuts.Name("specialPaste", default: Shortcut(.v, modifiers: [.command, .control]))
+}
