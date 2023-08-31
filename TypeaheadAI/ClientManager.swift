2ca79fb91b83e1e2590a7c3ad62f736ee0c2d310
kchro3 <jhara1418@gmail.com>
2023-08-30 21:53:38 -0700

    wip with semaphores

--- TypeaheadAI/ClientManager.swift
+++ TypeaheadAI/ClientManager.swift
@@ -5,6 +5,7 @@
 //  Created by Jeff Hara on 8/20/23.
 //
 
+import AppKit
 import Foundation
 import os.log
 
@@ -24,6 +25,11 @@ struct ResponsePayload: Codable {
     let errorMessage: String?
 }
 
+enum HTTPStatusCode: Int {
+    case ok = 200
+    case clientError = 400
+}
+
 enum ClientManagerError: Error {
     case badRequest(_ message: String)
     case retriesExceeded(_ message: String)
@@ -35,6 +41,10 @@ class ClientManager {
     private let session: URLSession
 
     private let apiUrl = URL(string: "https://typeahead-ai.fly.dev/get_response")!
+    private let semaphore = DispatchSemaphore(value: 5) // max 5 concurrent requests
+    private static let defaultRetryCount = 3
+    private static let defaultInitialDelay: TimeInterval = 1.0
+    private static let defaultTimeoutInSecs: TimeInterval = 10
 
     private let logger = Logger(
         subsystem: "ai.typeahead.TypeaheadAI",
@@ -86,94 +96,106 @@ class ClientManager {
         url: String,
         activeAppName: String,
         activeAppBundleIdentifier: String,
-        retryCount: Int = 3,
-        delay: TimeInterval = 1,
-        timeout: TimeInterval = 10,
+        retryCount: Int = defaultRetryCount,
+        delay: TimeInterval = defaultInitialDelay,
+        timeout: TimeInterval = defaultTimeoutInSecs,
         completion: @escaping (Result<String, Error>) -> Void
     ) {
-        // Obtain the queue designated for this request based on its UUID.
-        let queue = getQueue(for: id)
+        // NOTE: nested function for convenient recursion
+        func underlyingRequest(retryCount: Int, delay: TimeInterval) {
+            // Obtain the queue designated for this request based on its UUID.
+            let queue = getQueue(for: id)
 
-        // Dispatch the request logic to the designated queue.
-        queue.async {
-            let payload = RequestPayload(
-                username: username,
-                userFullName: userFullName,
-                userObjective: userObjective,
-                copiedText: copiedText,
-                url: url,
-                activeAppName: activeAppName,
-                activeAppBundleIdentifier: activeAppBundleIdentifier
-            )
+            // Dispatch the request logic to the designated queue.
+            queue.async {
+                let payload = RequestPayload(
+                    username: username,
+                    userFullName: userFullName,
+                    userObjective: userObjective,
+                    copiedText: copiedText,
+                    url: url,
+                    activeAppName: activeAppName,
+                    activeAppBundleIdentifier: activeAppBundleIdentifier
+                )
 
-            // Encode the RequestPayload instance into JSON data.
-            guard let httpBody = try? JSONEncoder().encode(payload) else {
-                completion(.failure(ClientManagerError.badRequest("Encoding error")))
-                return
-            }
-
-            // Create an HTTP POST request with the API URL and encoded payload.
-            var request = URLRequest(url: self.apiUrl, timeoutInterval: timeout)
-            request.httpMethod = "POST"
-            request.httpBody = httpBody
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-            // Initialize a URLSession data task with the request.
-            let task = self.session.dataTask(with: request) { (data, response, error) in
-                // Check for network errors.
-                if let error = error {
-                    // Retry logic: if retry count is not exhausted, retry after a delay.
-                    if retryCount > 0 {
-                        DispatchQueue.global().asyncAfter(deadline: .now() + delay) {
-                            self.logger.debug("Retrying... Remaining attempts: \(retryCount)")
-                            self.sendRequest(
-                                id: id,
-                                username: username,
-                                userFullName: userFullName,
-                                userObjective: userObjective,
-                                copiedText: copiedText,
-                                url: url,
-                                activeAppName: activeAppName,
-                                activeAppBundleIdentifier: activeAppBundleIdentifier,
-                                retryCount: retryCount - 1,
-                                delay: delay * 2,
-                                completion: completion
-                            )
-                        }
-                    } else {
-                        // Max retry attempts reached, invoke the completion handler with failure.
-                        self.logger.debug("Max retry attempts reached. Error: \(error.localizedDescription)")
-                        completion(.failure(ClientManagerError.retriesExceeded("Max retry attempts reached")))
-                    }
+                // Encode the RequestPayload instance into JSON data.
+                guard let httpBody = try? JSONEncoder().encode(payload) else {
+                    completion(.failure(ClientManagerError.badRequest("Encoding error")))
                     return
                 }
 
-                // Process the server's response.
-                if let data = data {
-                    do {
-                        let decodedResponse = try JSONDecoder().decode(ResponsePayload.self, from: data)
+                // Create an HTTP POST request with the API URL and encoded payload.
+                var request = URLRequest(url: self.apiUrl, timeoutInterval: timeout)
+                request.httpMethod = "POST"
+                request.httpBody = httpBody
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
-                        switch decodedResponse.responseCode {
-                        case 200:
-                            self.logger.debug("OK")
-                            completion(.success(decodedResponse.textToPaste))
-                        case 400:
-                            self.logger.debug("Client error")
-                            completion(.failure(ClientManagerError.clientError(decodedResponse.errorMessage ?? "")))
-                        default:
-                            self.logger.debug("Server error")
-                            completion(.failure(ClientManagerError.serverError(decodedResponse.errorMessage ?? "")))
+                // Initialize a URLSession data task with the request.
+                let task = self.session.dataTask(with: request) { (data, response, error) in
+                    // Check for network errors.
+                    if let error = error {
+                        handleError(error, retryCount: retryCount, delay: delay)
+                        return
+                    }
+
+                    // Process the server's response.
+                    if let data = data {
+                        do {
+                            self.semaphore.signal()
+                            let decodedResponse = try JSONDecoder().decode(ResponsePayload.self, from: data)
+                            if let statusCode = HTTPStatusCode(rawValue: decodedResponse.responseCode) {
+                                switch statusCode {
+                                case .ok:
+                                    completion(.success(decodedResponse.textToPaste))
+                                case .clientError:
+                                    completion(.failure(ClientManagerError.clientError(decodedResponse.errorMessage ?? "")))
+                                }
+                            } else {
+                                completion(.failure(ClientManagerError.serverError(decodedResponse.errorMessage ?? "")))
+                            }
+                        } catch {
+                            // An error occurred while decoding the response, invoke the completion handler with failure.
+                            self.logger.debug("Failed to process response: \(error.localizedDescription)")
+                            completion(.failure(error))
                         }
-                    } catch {
-                        // An error occurred while decoding the response, invoke the completion handler with failure.
-                        self.logger.debug("Failed to process response: \(error.localizedDescription)")
-                        completion(.failure(error))
                     }
                 }
-            }
 
-            // Start the URLSession data task.
-            task.resume()
+                // Start the URLSession data task.
+                task.resume()
+            }
+        }
+
+        // NOTE: nested error and retry handler
+        func handleError(_ error: Error, retryCount: Int, delay: TimeInterval) {
+            if retryCount > 0 {
+                DispatchQueue.global().asyncAfter(deadline: .now() + delay) {
+                    self.logger.debug("Retrying... Remaining attempts: \(retryCount)")
+                    underlyingRequest(retryCount: retryCount - 1, delay: delay * 2)
+                }
+            } else {
+                semaphore.signal()
+                self.logger.debug("Max retry attempts reached. Error: \(error.localizedDescription)")
+                completion(.failure(ClientManagerError.retriesExceeded("Max retry attempts reached")))
+            }
+        }
+
+        // Use an async function to handle the request
+        async {
+            if semaphore.wait(wallTimeout: .distantFuture) == .success {
+                defer { semaphore.signal() }  // Ensure semaphore is signaled even if we exit early
+
+                underlyingRequest(retryCount: 3, delay: 1)
+            } else {
+                // Max concurrency reached
+                DispatchQueue.main.async {
+                    let alert = NSAlert()
+                    alert.messageText = "Max Concurrency Reached"
+                    alert.informativeText = "Please wait before trying again."
+                    alert.alertStyle = .warning
+                    alert.runModal()
+                }
+            }
         }
     }
 }
