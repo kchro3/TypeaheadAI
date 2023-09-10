66745dc2c7c733e303595a2dd43161bd5f0ecced
kchro3 <jhara1418@gmail.com>
2023-09-09 22:36:23 -0700

    major refactoring of modal manager

--- TypeaheadAI/TypeaheadAIApp.swift
+++ TypeaheadAI/TypeaheadAIApp.swift
@@ -21,8 +21,6 @@ final class AppState: ObservableObject {
     @Published var isBlinking: Bool = false
     @Published var incognitoMode: Bool = false
 
-    private var toastWindow: NSWindow?
-
     private var blinkTimer: Timer?
     private let logger = Logger(
         subsystem: "ai.typeahead.TypeaheadAI",
@@ -86,12 +84,12 @@ final class AppState: ObservableObject {
         mouseEventMonitor.onLeftMouseDown = { [weak self] in
             // If the toast window is open and the user clicks out,
             // we can close the window.
-            if let window = self?.toastWindow {
+            if let window = self?.modalManager.toastWindow {
                 let mouseLocation = NSEvent.mouseLocation
                 let windowRect = window.frame
 
                 if !windowRect.contains(mouseLocation) {
-                    self?.toastWindow?.close()
+                    self?.modalManager.toastWindow?.close()
                     self?.clientManager.cancelStreamingTask()
                 }
             }
@@ -109,11 +107,6 @@ final class AppState: ObservableObject {
     }
 
     func specialCopy() {
-        if self.checkForTooManyRequests() {
-            self.logger.debug("special copy is disabled")
-            return
-        }
-
         checkAndRequestAccessibilityPermissions()
 
         self.logger.debug("special copy")
@@ -132,19 +125,20 @@ final class AppState: ObservableObject {
                 // NOTE: If the modal is empty but the clipboard is not,
                 // whatever was in the clipboard initially is from a regular
                 // copy, in which case we just do the regular flow.
-                if let window = self.toastWindow, window.isVisible {
+                if let window = self.modalManager.toastWindow, window.isVisible {
                     window.close()
                 } else {
-                    self.showSpecialCopyModal()
+                    self.modalManager.showSpecialCopyModal()
                 }
             } else {
                 // Clear the modal text and reissue request
                 self.modalManager.clearText()
-                self.showSpecialCopyModal()
+                self.modalManager.showSpecialCopyModal()
                 self.clientManager.predict(
                     id: UUID(),
                     copiedText: copiedText,
-                    incognitoMode: self.incognitoMode
+                    incognitoMode: self.incognitoMode,
+                    stream: true
                 ) { result in
                     switch result {
                     case .success(let chunk):
@@ -410,111 +404,6 @@ final class AppState: ObservableObject {
             if let error = error {
                 self.logger.error("Failed to send notification: \(error.localizedDescription)")
             }
-        }
-    }
-
-    private func showSpecialCopyModal() {
-        toastWindow?.close()
-
-        // Create the visual effect view with frosted glass effect
-        let visualEffect = NSVisualEffectView()
-        visualEffect.blendingMode = .behindWindow
-        visualEffect.state = .active
-        visualEffect.material = .hudWindow
-
-        // Create the content view
-        let contentView = ModalView(
-            showModal: .constant(true),
-            copyModalManager: self.modalManager
-        )
-
-        let hostingView = NSHostingView(rootView: contentView)
-        hostingView.translatesAutoresizingMaskIntoConstraints = false
-
-        // Create a base view to hold both the visualEffect and hostingView
-        let baseView = NSView()
-
-        // Create the window
-        toastWindow = CustomModalWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 300, height: 200),
-            styleMask: [
-                .closable,
-                .fullSizeContentView,
-                .resizable,
-                .titled
-            ],
-            backing: .buffered,
-            defer: false
-        )
-
-        // Set the base effect view as the window's content view
-        toastWindow?.contentView = baseView
-
-        // Now that baseView has a frame, set the frames for visualEffect and hostingView
-        visualEffect.frame = baseView.bounds
-        visualEffect.autoresizingMask = [.width, .height]
-
-        hostingView.frame = baseView.bounds
-
-        // Add visualEffect and hostingView to baseView
-        baseView.addSubview(visualEffect)
-        baseView.addSubview(hostingView)
-
-        // Add constraints to make the hosting view fill the base view
-        NSLayoutConstraint.activate([
-            hostingView.topAnchor.constraint(equalTo: baseView.topAnchor),
-            hostingView.bottomAnchor.constraint(equalTo: baseView.bottomAnchor),
-            hostingView.leadingAnchor.constraint(equalTo: baseView.leadingAnchor),
-            hostingView.trailingAnchor.constraint(equalTo: baseView.trailingAnchor)
-        ])
-
-        // Set the x, y coordinates and the size to the user's last preference or the center by default
-        let x = UserDefaults.standard.value(forKey: "toastWindowX") as? CGFloat
-        let y = UserDefaults.standard.value(forKey: "toastWindowY") as? CGFloat
-        let width = UserDefaults.standard.value(forKey: "toastWindowSizeWidth") as? CGFloat
-        let height = UserDefaults.standard.value(forKey: "toastWindowSizeHeight") as? CGFloat
-
-        if let x = x, let y = y, let width = width, let height = height {
-            toastWindow?.setFrame(NSRect(x: x, y: y, width: width, height: height), display: true)
-        } else {
-            toastWindow?.setFrame(NSRect(x: 0, y: 0, width: 300, height: 200), display: true)
-            toastWindow?.center()
-        }
-
-        toastWindow?.titlebarAppearsTransparent = true
-        toastWindow?.isMovableByWindowBackground = true
-        toastWindow?.isReleasedWhenClosed = false
-        toastWindow?.level = .popUpMenu
-        toastWindow?.makeKeyAndOrderFront(nil)
-
-        // Register for window moved notifications to save the new position
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(windowDidMove(_:)),
-            name: NSWindow.didMoveNotification, object: toastWindow)
-
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(windowDidResize(_:)),
-            name: NSWindow.didResizeNotification, object: toastWindow)
-    }
-
-    @objc func windowDidMove(_ notification: Notification) {
-        if let movedWindow = notification.object as? NSWindow {
-            let origin = movedWindow.frame.origin
-
-            UserDefaults.standard.set(origin.x, forKey: "toastWindowX")
-            UserDefaults.standard.set(origin.y, forKey: "toastWindowY")
-        }
-    }
-
-    @objc func windowDidResize(_ notification: Notification) {
-        if let movedWindow = notification.object as? NSWindow {
-            let size = movedWindow.frame.size
-
-            UserDefaults.standard.set(size.width, forKey: "toastWindowSizeWidth")
-            UserDefaults.standard.set(size.height, forKey: "toastWindowSizeHeight")
-            print("Saved width: \(size.width), height: \(size.height)")
         }
     }
 }
