0953b38b7d8a613a1a622116238fcb31cbcd35b7
kchro3 <jhara1418@gmail.com>
2023-10-06 13:31:56 -0700

    fuck me

--- TypeaheadAI/ClientManager.swift
+++ TypeaheadAI/ClientManager.swift
@@ -135,17 +135,17 @@ class ClientManager {
 
     private let session: URLSession
 
-//    private let apiUrlStreaming = URL(string: "https://typeahead-ai.fly.dev/v2/get_stream")!
-//    private let apiOnboarding = URL(string: "https://typeahead-ai.fly.dev/onboarding")!
-//    private let apiImage = URL(string: "https://typeahead-ai.fly.dev/v2/get_image")!
-//    private let apiIntents = URL(string: "https://typeahead-ai.fly.dev/v2/suggest_intents")!
-//    private let apiImageCaptions = URL(string: "https://typeahead-ai.fly.dev/v2/get_image_caption")!
+    private let apiUrlStreaming = URL(string: "https://typeahead-ai.fly.dev/v2/get_stream")!
+    private let apiOnboarding = URL(string: "https://typeahead-ai.fly.dev/onboarding")!
+    private let apiImage = URL(string: "https://typeahead-ai.fly.dev/v2/get_image")!
+    private let apiIntents = URL(string: "https://typeahead-ai.fly.dev/v2/suggest_intents")!
+    private let apiImageCaptions = URL(string: "https://typeahead-ai.fly.dev/v2/get_image_caption")!
 
-    private let apiUrlStreaming = URL(string: "http://localhost:8080/v2/get_stream")!
-    private let apiOnboarding = URL(string: "http://localhost:8080/onboarding")!
-    private let apiImage = URL(string: "http://localhost:8080/v2/get_image")!
-    private let apiIntents = URL(string: "http://localhost:8080/v2/suggest_intents")!
-    private let apiImageCaptions = URL(string: "http://localhost:8080/v2/get_image_caption")!
+//    private let apiUrlStreaming = URL(string: "http://localhost:8080/v2/get_stream")!
+//    private let apiOnboarding = URL(string: "http://localhost:8080/onboarding")!
+//    private let apiImage = URL(string: "http://localhost:8080/v2/get_image")!
+//    private let apiIntents = URL(string: "http://localhost:8080/v2/suggest_intents")!
+//    private let apiImageCaptions = URL(string: "http://localhost:8080/v2/get_image_caption")!
 
     private let logger = Logger(
         subsystem: "ai.typeahead.TypeaheadAI",
