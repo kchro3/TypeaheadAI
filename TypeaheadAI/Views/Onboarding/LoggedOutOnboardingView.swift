45d1a298b26d90cfcb832559c2967b049417eb5e
kchro3 <jhara1418@gmail.com>
2023-11-11 23:22:08 -0800

    begin working on new onboarding flow

---
+++ TypeaheadAI/Views/Onboarding/LoggedOutOnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoggedOutOnboardingView.swift
+//  TypeaheadAI
+//
+//  Created by Jeff Hara on 11/11/23.
+//
+
+import SwiftUI
+
+struct LoggedOutOnboardingView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LoggedOutOnboardingView()
+}
