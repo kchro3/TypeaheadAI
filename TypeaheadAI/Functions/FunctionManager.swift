fc5cb51bfd9fc7f2d9fc14b6d0ce9abaddedd95a
kchro3 <jhara1418@gmail.com>
2023-11-21 00:20:50 -0800

    wip for opening apps... not sure it makes sense anymore

--- TypeaheadAI/Functions/FunctionManager.swift
+++ TypeaheadAI/Functions/FunctionManager.swift
@@ -26,17 +26,60 @@ struct FunctionCall: Codable {
 }
 
 class FunctionManager: CanFetchAppContext, CanSimulateSelectAll, CanSimulateCopy, CanSimulateClose {
-    func openURL(_ url: String) async throws {
+    func openURL(_ url: String, prompt: String, modalManager: ModalManager) async throws {
+        // Fetch the original app context
+        let appContext = try await fetchAppContext()
+
+        // Add URL to message
+        await modalManager.appendText("Opening [\(url)](\(url))...")
+
+        // Open URL
         guard let url = URL(string: url) else {
             throw FunctionError.openURL("URL not found")
         }
 
         NSWorkspace.shared.open(url)
+
+        // Close window temporarily
+        await modalManager.closeModal()
+
+        // Let the page load for 5 seconds
+        try await Task.sleep(for: .seconds(5))
+
+        // Simulate selecting -> copying -> closing the window
+        try await simulateSelectAll()
+        try await simulateCopy()
+        try await simulateClose()
+
+        // Return to the original app
+        if let bundleIdentifier = appContext?.bundleIdentifier,
+           let app = NSRunningApplication.runningApplications(withBundleIdentifier: bundleIdentifier).first {
+            // Activate the app, bringing it to the foreground
+            app.activate(options: [.activateIgnoringOtherApps])
+        }
+
+        // Show the window
+        await modalManager.showModal()
+
+        // HIDDEN messages under the hood
+        if let copiedText = NSPasteboard.general.string(forType: .string) {
+            await modalManager.setText("Here's what I copied from \(url):\n\(copiedText)", isHidden: true)
+        }
+        await modalManager.setUserMessage(prompt, isHidden: true)
+
+        // Reply
+        await modalManager.replyToUserMessage(refresh: false)
+    }
+
+    func openApp(_ appName: String, _ bundleIdentifier: String, prompt: String, modalManager: ModalManager) async throws {
+        // Fetch the original app context
+        let appContext = try await fetchAppContext()
+
+        // Add URL to message
+        await modalManager.appendText("Opening \(appName)...")
     }
 
     func parseAndCallFunction(jsonString: String, modalManager: ModalManager) async throws {
-        let appContext = try await fetchAppContext()
-
         guard let jsonData = jsonString.data(using: .utf8),
               let functionCall = try? JSONDecoder().decode(FunctionCall.self, from: jsonData) else {
             return
@@ -44,34 +87,24 @@ class FunctionManager: CanFetchAppContext, CanSimulateSelectAll, CanSimulateCopy
 
         switch functionCall.name {
         case "open_url":
-            
+            // Check arguments
             guard let url = functionCall.args["url"], let prompt = functionCall.args["prompt"] else {
                 await modalManager.setError("Failed to open url...")
                 return
             }
 
-            await modalManager.appendText("Opening \(functionCall.args["url"] ?? "url")...")
-            try await openURL(functionCall.args["url"]!)
-            await modalManager.closeModal()
-            try await Task.sleep(for: .seconds(3))
-            try await simulateSelectAll()
-            try await simulateCopy()
-            try await simulateClose()
+            try await openURL(url, prompt: prompt, modalManager: modalManager)
+        case "open_app":
+            // Check arguments
+            guard let app = functionCall.args["app"], 
+                  let bundleIdentifier = functionCall.args["bundleIdentifier"],
+                  let prompt = functionCall.args["prompt"] else {
 
-            if let bundleIdentifier = appContext?.bundleIdentifier,
-               let app = NSRunningApplication.runningApplications(withBundleIdentifier: bundleIdentifier).first {
-                    // Activate the app, bringing it to the foreground
-                    app.activate(options: [.activateIgnoringOtherApps])
+                await modalManager.setError("Failed to open app...")
+                return
             }
-
-            await modalManager.showModal()
-
-            if let copiedText = NSPasteboard.general.string(forType: .string) {
-                await modalManager.setText("Here's what I copied from \(url):\n\(copiedText)", isHidden: true)
-            }
-
-            await modalManager.setUserMessage(prompt, isHidden: true)
-            await modalManager.replyToUserMessage(refresh: false)
+            
+            try await openApp(app, bundleIdentifier, prompt: prompt, modalManager: modalManager)
         default:
             throw FunctionError.notFound("Function \(functionCall.name) not found.")
         }
