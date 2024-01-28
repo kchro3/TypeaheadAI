5fc96b7d8c58230c27e4989c938aef7d77d6fdc3
kchro3 <jhara1418@gmail.com>
2024-01-28 12:13:41 -0800

    update

--- .github/scripts/update_localizations.py
+++ .github/scripts/update_localizations.py
@@ -1,6 +1,6 @@
 import argparse
+import collections
 import json
-import sys
 import os
 
 
@@ -8,7 +8,23 @@ assert os.environ["OPENAI_API_KEY"] is not None
 
 
 def main(file_path):
-    print(file_path)
+    with open(file_path, 'r') as f:
+        strings = json.load(f, object_pairs_hook=collections.OrderedDict)
+
+    ## handle changes
+    stale_keys = []
+    for i, (key, value) in enumerate(strings['strings'].items()):
+        if 'extractionState' in value and value['extractionState'] == 'stale':
+            stale_keys.append(key)
+
+    for key in stale_keys:
+        del strings['strings'][key]
+    ## 
+
+    with open(file_path, 'w') as w:
+        raw = json.dumps(strings, ensure_ascii=False, indent=2, separators=(',', ' : '))
+        raw = raw.replace('{},\n', '{\n\n    },\n')  # overwrite if there's an empty dictionary
+        w.write(raw)
 
 
 if __name__ == "__main__":
